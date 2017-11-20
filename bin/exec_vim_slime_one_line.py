f25ef381926a7733e0e410b03b3576edc51552e5
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2017-11-20 13:31:24 +0100

    Bind Hyper-e Hyper-E for vim Slime

---
+++ bin/exec_vim_slime_one_line.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from pyautogui import press, typewrite
+press('esc')
+typewrite(':SlimeSendCurrentLine')
+press('enter')
---
+++ bin/exec_vim_slime_paragraph.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from pyautogui import press, typewrite
+press('esc')
+typewrite('vip')
+typewrite(':SlimeSend')
+press('enter')
