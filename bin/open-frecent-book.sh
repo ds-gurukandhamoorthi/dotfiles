46af07cdb3ed7585c939257278eaf5f1388d6dcf
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2018-12-21 16:23:20 +0100

    Open second most frecent pdf with minimum number of pages criterion

--- bin/open-frecent-book.sh
+++ bin/open-frecent-book.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+SKIP=${1:-0}
 FRECENT_BOOKS=$(fasd -flR pdf)
 # Minimum number of pages to be considered as a book
 MIN_PAGES_OF_BOOK=2
 
+nb_pages_pdf () {
+    local book=$1
+    local nb_pages=$(pdfinfo "$book" | grep Pages | cut -d: -f2 | tr -d ' ')
+    echo $nb_pages
+}
+
 echo "$FRECENT_BOOKS" | while read book; do
     echo "$book"
-    nb_pages=$(pdfinfo "$book" | grep Pages | cut -d: -f2 | tr -d ' ')
-    if (( nb_pages >= MIN_PAGES_OF_BOOK )); then
-        zathura "$book"
-        break
+    if (( $(nb_pages_pdf "$book") >= MIN_PAGES_OF_BOOK )); then
+        if (( $SKIP == 0 )); then
+            zathura "$book"
+            break
+        else
+            ((SKIP-=1))
+        fi
     fi
     #if none of the pdfs contain more than MIN_PAGES_OF_BOOK then nothing shall be opened
 done
