9edaf571e4f3703aec97401e87a4fb4759cffbfa
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-05 16:44:27 +0200

    Modify openWithVlc script to use up and down keys

--- bin/openWithVlc.sh
+++ bin/openWithVlc.sh
@@ -10,8 +10,7 @@ then
         --key-nav-activate "None" --key-play-pause 'Enter' \
         --key-nav-left "None" --key-jump-extrashort "Left" \
         --key-nav-right "None" --key-jump+extrashort "Right" \
-        --key-jump-short "Page Up" \
-        --key-jump+short "Page Down"\
+        --key-jump-short "Up" \
+        --key-jump+short "Down"'
         "$FILE"
 fi
-
