fd59108e5183710e6cdc21d66b55e1bf05394de7
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-14 11:03:49 +0200

    Modify video player command to incorporate regularly used options

--- bin/open-frecent-and-related-with-video-player.sh
+++ bin/open-frecent-and-related-with-video-player.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-VIDEO_PLAYER=cvlc
 FILE=$(python3 /home/guru/bin/open-frecent-and-related.py --file-type-video)
 echo $FILE
 if [ -n "$FILE" ]
 then
     fasd -A "$FILE"
-    $VIDEO_PLAYER "$FILE"
+    cvlc --play-and-exit --hotkeys-mousewheel-mode 1\
+        --key-intf-popup-menu "None" --key-frame-next "Menu"\
+        --key-nav-activate "None" --key-play-pause 'Enter' \
+        --key-nav-left "None" --key-jump-extrashort "Left" \
+        --key-nav-right "None" --key-jump+extrashort "Right" \
+        --key-jump-short "Page Up" \
+        --key-jump+short "Page Down"\
+        --key-zoom "None"\
+        --key-aspect-ratio "None"\
+        --key-crop "None"\
+        "$FILE"
 fi
